a71d63ce172223acb6406ab8a481f79faccda82b
Guifré Puig Ribas <guifreribas@gmail.com>
2024-10-16 15:08:09 +0200

    Fix short URL retrieval logic and improve error handling in the API for better response consistency and clarity

--- src/app/api/[shortUrl]/route.js
+++ src/app/api/[shortUrl]/route.js
@@ -1,23 +1,24 @@
 import { NextResponse } from "next/server";
-import clientPromise from "../lib/mongodb";
+import clientPromise from "@/app/lib/mongodb";
 
 export async function GET(request, { params }) {
 	const { shortUrl } = params;
 
 	try {
 		const client = await clientPromise;
-		const db = client.db("shortUrl");
+		const db = client.db("shorturl");
 
-		const urlEntry = await db.collection("urls").findOne({ shortUrl });
+		const urlEntry = await db
+			.collection("urls")
+			.findOne({ shortUrl: String(shortUrl) });
 
-		if (urlEntry) {
-			return NextResponse.redirect(urlEntry.originalUrl, 301);
-		} else {
+		if (!urlEntry) {
 			return NextResponse.json(
 				{ message: "URL no trobada" },
 				{ status: 404 }
 			);
 		}
+		return NextResponse.redirect(urlEntry.originalUrl, 301);
 	} catch (error) {
 		console.error(error);
 		return NextResponse.json(
