6bd226d342a4065ab0c0adbbf394d16cc2b06ac6
Guifré Puig Ribas <guifreribas@gmail.com>
2024-10-24 01:49:28 +0200

    style: enhance homepage layout and content for improved user experience and clarity on service limitations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,19 @@ import ShortUrlForm from "../components/ShortUrl";
 
 export default function Home() {
   return (
-    <div>
+    <article className="flex flex-col justify-center items-center gap-4 min-h-screen bg-gray-100 dark:bg-gray-800 border">
+
+      <section className="flex flex-col max-w-4xl w-fullbg-white gap-2 p-8 dark:bg-gray-900 rounded-lg shadow-md">
+        <h1 className="text-center font-bold text-2xl md:text-5xl">Guifre Link</h1>
+        <p className="text-center text-gray-600 dark:text-gray-400">
+          Simplifica i escurça les teves URLs de manera fàcil i ràpida amb la
+          nostra eina gratuïta. Comparteix enllaços més curts i eficients amb un
+          sol clic.
+        </p>
+        <p className="text-lg text-center font-bold underline">Actualment està en una fase de desenvolupament, així que la teva url pot ser que duri màxim 24hs.</p>
+        <p className="text-center">Agraeixo la teva comprensió i espero que gaudeixis de la meva eina.</p>
+      </section>
       <ShortUrlForm />
-    </div>
+    </article>
   );
 }
--- src/components/ShortUrl.tsx
+++ src/components/ShortUrl.tsx
@@ -30,7 +30,7 @@ export default function ShortUrlForm() {
 	};
 
 	return (
-		<div className='flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-800'>
+		<div className='w-fit mx-auto flex items-center justify-center bg-gray-100 dark:bg-gray-800'>
 			<div className='max-w-lg w-full bg-white dark:bg-gray-900 rounded-lg shadow-md p-6'>
 				<h1 className='text-2xl font-bold mb-4 text-center'>
 					{`Retallador d'URL`}
