88eb5f20de354cb6313498d2f3b011027b072a9f
Guifré Puig Ribas <guifreribas@gmail.com>
2024-10-24 02:32:50 +0200

    style: update homepage paragraph font weight for improved readability and visual consistency in the overall design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <section className="flex flex-col max-w-4xl w-fullbg-white gap-2 p-8 dark:bg-gray-900 rounded-lg shadow-md">
         <h1 className="text-center font-bold text-2xl md:text-5xl">Guifre Link</h1>
         <p className="text-lg text-center font-bold underline">Actualment està en una fase de desenvolupament, així que la teva url pot ser que duri màxim 24hs.</p>
-        <p className="text-center">Agraeixo la teva comprensió i espero que gaudeixis de la meva eina.</p>
+        <p className="text-center font-extralight">Agraeixo la teva comprensió i espero que gaudeixis de la meva eina.</p>
       </section>
       <ShortUrlForm />
     </article>
